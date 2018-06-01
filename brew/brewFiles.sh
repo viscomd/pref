7c6f2e5334dedd4a5093cb118a0fe0be9436c56c
David Viscomi <djviscomi@gmail.com>
2018-06-01 08:07:24 -0400

    adding changes to brewfiles

--- brew/brewFiles.sh
+++ brew/brewFiles.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+#standard brew files
+#-------------------
+
+#Essential
+#---------
 brew install bash
 brew install vim
+#Non Essential
+#-------------
 brew install wget
 brew install the_silver-searcher
 brew install tree
+
+#Cask Brew Files
+#---------------
 brew tap caskroom/versions
 brew cask install sublime-text
 brew cask install spotify
+brew cask install slack
 brew tap caskroom/fonts
 
 

f3c28413f67b360bfa89e7cd3622f3c14f86be2a
David Viscomi <david.viscomi@cbc.ca>
2019-07-09 07:56:18 -0400

    alphabetizing the brewfilew

--- brew/brewFiles.sh
+++ brew/brewFiles.sh
@@ -9,21 +9,23 @@ brew install bash
 brew install vim
 #Non Essential
 #-------------
-brew install wget
-brew install the_silver-searcher
-brew install tree
 brew install bash-completion
-brew install tig
-brew install thefuck
 brew install peco
+brew install the_silver-searcher
+brew install thefuck
+brew install tig
+brew install tree
+brew install wget
+
 
 
 #Cask Brew Files
 #---------------
 brew tap caskroom/versions
-brew cask install sublime-text
-brew cask install spotify
-brew cask install slack
 brew tap caskroom/fonts
+
+brew cask install slack
+brew cask install spotify
+brew cask install sublime-text
 
 
